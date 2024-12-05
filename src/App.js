eaf3deb1e34f8dc7591c196a4ec349494f061cec
Jose Davila <josedavilasalcedo97@gmail.com>
2024-12-05 17:59:16 -0500

    se agrego el componente equipo

--- src/App.js
+++ src/App.js
@@ -4,23 +4,69 @@ import Header from './componentes/Header/Header';
 import Formulario from './componentes/Formulario/Formulario';
 import MiOrg from './componentes/MiOrg';
 import { useState } from 'react';
+import Equipo from './componentes/Equipo';
 
 function App() {
 
-  const [mostrarFormulario, actualizarMostrar] = useState(true)
+  const [mostrarFormulario, actualizarMostrar] = useState(false)
 
   //Ternario --> condicion ? seMuestra : noSeMuestra
   const cambiarMostrar = () => {
     actualizarMostrar (!mostrarFormulario)
   }
 
+  //Lista de equipos
+  const equipos = [
+    {
+      titulo: "Programación",
+      colorPrimario: "#57C278 ",
+      colorSecundario: "#D9F7E9"
+    },
+    {
+      titulo: "Front End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    },
+    {
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    },
+    {
+      titulo: "Devops",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    },
+    {
+      titulo: "UX y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
+    },
+    {
+      titulo: "Móvil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    },
+    {
+      titulo: "Innovación y  Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    },
+
+  ]
+
   return (
-    <div >
+    <>
       <Header />
       {/* {mostrarFormulario ?   <Formulario /> : <></> } */}
       {mostrarFormulario && <Formulario />}
       <MiOrg cambiarMostrar={cambiarMostrar}/>
-    </div>
+
+      {
+        equipos.map((equipo) => <Equipo datos={equipo} key={equipo.titulo}/>)
+      }
+
+    </>
   );
 }
 

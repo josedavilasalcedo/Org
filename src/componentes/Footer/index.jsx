2eff9037906d6ffc4d2e27ef4857373f725c470d
Jose Davila <josedavilasalcedo97@gmail.com>
2024-12-12 10:38:01 -0500

    correcion de etiquetas y links en footer

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,20 +1,23 @@
 import "./Footer.css"
 
 const Footer = () => {
-    return <footer className="footer" style={{backgroundImage: "url(/img/footer.png)"}}>
-        <di className="redes">
-            <a href="www.aluracursos.com">
-                <img src="/img/facebook.png" alt="facebook"/>
+    return <footer className="footer" style={{ backgroundImage: "url(/img/footer.png)" }}>
+        <div className="redes">
+            <a href="https://www.aluracursos.com">
+
+                <img src="/img/facebook.png" alt="facebook" />
             </a>
 
-            <a href="www.aluracursos.com">
-                <img src="/img/twitter.png" alt="facebook"/>
+            <a href="https://www.aluracursos.com">
+
+                <img src="/img/twitter.png" alt="facebook" />
             </a>
 
-            <a href="www.aluracursos.com">
-                <img src="/img/instagram.png" alt="facebook"/>
+            <a href="https://www.aluracursos.com">
+
+                <img src="/img/instagram.png" alt="facebook" />
             </a>
-        </di>
+        </div>
         <img src="/img/Logo.png" alt="org" />
         <strong>Desarrollado por Jose Davila</strong>
     </footer>
